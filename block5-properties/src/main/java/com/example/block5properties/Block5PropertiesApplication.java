df94da974d84a9bb4a916286b853da6368749de1
rbernabeulopez <rafael.bernabeu@bosonit.com>
2022-11-21 09:00:42 +0100

    Finished properties exercise

---
+++ block5-properties/src/main/java/com/example/block5properties/Block5PropertiesApplication.java
@@ -0,0 +1,32 @@
+package com.example.block5properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Block5PropertiesApplication {
+
+    public static void main(String[] args) {
+        System.setProperty("new.value", "funciona");
+        SpringApplication.run(Block5PropertiesApplication.class, args);
+    }
+
+    @Value("${greeting}")
+    private String greeting;
+
+    @Value("${my.number}")
+    private String myNumber;
+
+    @Value("${new.value}")
+    private String newProperty;
+
+    @Bean
+    public void imprimePropiedades() {
+        System.out.println("El valor de greeting es: (valor de ‘" + greeting + "’)");
+        System.out.println("El valor de my.number es: (valor de ‘" +myNumber + "’)");
+        System.out.println("El valor de newProperty es: (valor de ‘" +newProperty + "’)");
+    }
+
+}
