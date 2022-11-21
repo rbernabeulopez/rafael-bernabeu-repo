bb833b088c8f582be2c145fa6009429d5ad0f2a3
rbernabeulopez <rafael.bernabeu@bosonit.com>
2022-11-21 09:58:24 +0100

    Created logging project

---
+++ block5-logging/src/main/java/com/example/block5logging/Block5LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.block5logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block5LoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Block5LoggingApplication.class, args);
+    }
+
+}
